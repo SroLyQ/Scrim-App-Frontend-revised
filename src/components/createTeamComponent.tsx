ff4160ec158d58337872b00d9f273b2facabe11f
kawjaw66 <kawjaw66@gmail.com>
2022-04-03 22:17:55 +0700

    +layout createTeam page

---
+++ src/components/createTeamComponent.tsx
@@ -0,0 +1,26 @@
+import CreateTeamForm from "./createTeamFormComponent"
+import MemberForm from "./MemberForm"
+
+const CreateTeamComponent : React.FC = () => {
+    return(
+        <div>
+            <div className="flex w-full bg-red-700 h-[100px] mt-[4%] pl-[4%] items-center">
+                <h1 className="text-4xl font-bold">
+                    Create Your Team
+                </h1>
+            </div>
+            <div className="flex w-full h-[680px] bg-blue-700 items-center justify-center">
+                <div className="flex flex-col w-[98%] h-[98%] bg-green-700">
+                    <div className="bg-yellow-700 w-full h-[50%] ">
+                        <CreateTeamForm/>
+                    </div>
+                    <div className="bg-gray-700 w-full h-[50%] ">
+                        <MemberForm/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateTeamComponent
--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,10 +1,59 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import HeaderComponent from "../components/HeaderComponent";
-
+import CreateTeamComponent from "../components/createTeamComponent";
+import { useRef } from "react";
 
 
 const TeamPage : React.FC = () =>{
     let {id} = useParams();
+    let navigate=useNavigate()
+    const createTeamClickHandler = () =>{
+        var path:string = './../createTeam'
+        navigate(path)
+    }
+
+    if(id === 'noteam'){
+        return(
+            <div>
+                <HeaderComponent/>
+                <div className="flex flex-rows">
+                    <div className="basis-1/5 bg-gray-400 h-[92vh] w-[100%]"> </div>
+                    <div className="basis-3/5 bg-white h-[92vh] text-center">
+                        <div className="flex flex-col justify-center items-center h-full w-full">
+                            <div className="bg-red-700">
+                                <h1 className="text-4xl font-bold ">You are currently LFT</h1>
+                                <div className="flex flex-rows " >
+                                    <button className="bg-blue-700" onClick={createTeamClickHandler}>
+                                        <h1 className="text-4xl font-bold mt-[4%]">Create Team </h1>
+                                    </button>
+                                    <h1 className="text-4xl font-bold mt-[4%]">&nbsp;or&nbsp;</h1>
+                                    <button className="bg-blue-700">
+                                        <h1 className="text-4xl font-bold mt-[4%]">Join Team </h1>
+                                    </button>
+                                    <h1 className="text-4xl font-bold mt-[4%]">&nbsp;Now!&nbsp;</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="basis-1/5 bg-gray-400 h-[92vh]"> </div>
+                </div>
+            </div>
+        )
+    }
+    else if(id == 'createTeam'){
+        return (
+            <div>
+                <HeaderComponent/>
+                <div className="flex flex-rows">
+                    <div className="basis-1/5 bg-gray-400 h-[92vh] w-[100%]"> </div>
+                    <div className="basis-3/5 bg-white h-[92vh]">
+                        <CreateTeamComponent/>
+                    </div>
+                    <div className="basis-1/5 bg-gray-400 h-[92vh]"> </div>
+                </div>
+            </div>
+        )
+    }
     return (
         <HeaderComponent/>
     )
